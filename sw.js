320575f51b1a40d81da64257f0894129c86ab59b
iofahmawi <mo.fahmawi@gmail.com>
2026-07-18 11:59:34 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // sw.js - Image Editor (Dynamic Version)
 
-const CACHE_NAME = 'image-editor-dynamic-v51';
+const CACHE_NAME = 'image-editor-dynamic-v52';
 
 // نخزن فقط ملف الواجهة لضمان نجاح التثبيت السريع
 const urlsToCache = [
